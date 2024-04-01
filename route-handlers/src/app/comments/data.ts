7fe05e001d06687326e8d9b69bc88d550dbcd955
Tulasi Joshua Mbawini <mbawinitulasi12@gmail.com>
2024-04-01 19:52:23 +0000

    Feat: route handler

---
+++ route-handlers/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1, 
+        text: "This is the first comment and"
+    },
+    {
+        id: 2, 
+        text: "This is the second comment go"
+    },
+    {
+        id: 3, 
+        text: "This is the third comment to"
+    },
+]
