0119b848dc66b0aabec0c79f7009d1a1e09f58fd
Tulasijoshua <mbawinitulasi12@gmail.com>
2024-06-27 05:41:50 +0000

    Feat: route handlers

---
+++ route-handler2/src/app/comments/data.ts
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        text: "This is the first commment"
+    },
+    {
+        id: 2,
+        text: "This is the second commment"
+    },
+    {
+        id: 3,
+        text: "This is the third commment"
+    },
+]
